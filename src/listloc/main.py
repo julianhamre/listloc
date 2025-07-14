df04101df75653c5a059dcfc17489cd0e890f257
Julian Hamre <julian.hamre5@gmail.com>
2025-07-14 09:54:54 +0200

    Add more descriptive cli docs

--- src/listloc/main.py
+++ src/listloc/main.py
@@ -1,15 +1,27 @@
 import typer
 from typing_extensions import Annotated
+from rich.markdown import Markdown
 import os
 from listloc.extractor.listing_extractor import ListingExtractor
 
 
-app = typer.Typer()
+app = typer.Typer(rich_markup_mode="rich", help="""
+Extract and manage code listings from source files.
+
+See: https://github.com/julianhamre/listloc for more examples and usage.
+""")
 
 @app.command()
 def extract(path: Annotated[str, typer.Argument()] = os.getcwd()):
     """
-    Extract all listings from the files under the given directory. If no path is provided, the current working directory is used.
+    Recursively extract all declared code listings from the source files under the given directory.
+
+    Each code listing is the content between `BEGIN LISTING <listing_name>` and `END LISTING`.
+    Extracted listings are saved as `.listing` files inside `listings/` directories.
+
+    If no directory path is provided, the current working directory is used.
+
+    Example: listloc extract ./my_project
     """
     extractor = ListingExtractor(path)
     extractor.extract_all_listings()
@@ -18,7 +30,13 @@ def extract(path: Annotated[str, typer.Argument()] = os.getcwd()):
 @app.command()
 def clear(path: Annotated[str, typer.Argument()] = os.getcwd()):
     """
-    Delete all extracted listing files under the given directory. Every listing directory only containing extracted listings will be deleted as well. If no path is provided, the current working directory is used.
+    Recursively delete all extracted `.listing` files under the given directory.
+
+    Any `listings/` directory that contains only `.listing` files will also be deleted.
+
+    If no path is provided, the current working directory is used.
+
+    Example: listloc clear ./my_project
     """
     extractor = ListingExtractor(path)
     extractor.clear_all_listing_extractions()
