3a4355113fbd1de7d04271ae51e5567ace6b17ac
Julian Hamre <julian.hamre5@gmail.com>
2025-12-18 21:31:20 +0100

    Make TestListingExtractor extend BaseTestListingExtractor

--- tests/listloc/extractor/test_listing_extractor.py
+++ tests/listloc/extractor/test_listing_extractor.py
@@ -3,14 +3,14 @@ import os
 from src.listloc.extractor.listing_extractor import ListingExtractor, FileExtractor
 from src.listloc.extractor.listing_constants import ListingConstants
 from tests.listloc.extractor.file_creation_tools import FileCreationTools
+from tests.listloc.extractor.base_test_listing_extractor import BaseTestListingExtractor
 from listloc.extractor.context.extractor_context import ExtractorContext
 import tempfile
 
-class TestListingExtractor(unittest.TestCase):
+class TestListingExtractor(BaseTestListingExtractor):
 
     def setUp(self):
-        self.temp_dir = tempfile.TemporaryDirectory()
-        self._BASE_DIRECTORY_PATH = self.temp_dir.name
+        super().setUp()
         self.__SUBDIRS = ["dir1", 
                           os.path.join("dir1", "dir2"), 
                           os.path.join("dir1", "dir3"), 
@@ -21,13 +21,7 @@ class TestListingExtractor(unittest.TestCase):
                         os.path.join("dir1", "dir2", "file22"): "file22_listing", 
                         os.path.join("dir1", "dir3", "dir4", "file41"): "file41_listing", 
                         os.path.join("dir1", "dir3", "dir4", "file2"): "file42_listing"}
-        self.__file_creator = FileCreationTools(self._BASE_DIRECTORY_PATH, self.__SUBDIRS, self.__FILES)
-        self.__create_test_file_structure()
-        self.__context = ExtractorContext(self._BASE_DIRECTORY_PATH)
-        self.__listing_extractor = ListingExtractor(self._BASE_DIRECTORY_PATH, self.__context)
-    
-    def __create_test_file_structure(self):
-        self.__file_creator.create_subdirs_and_code_files()
+        self._create_test_file_structure(self.__SUBDIRS, self.__FILES)
         self.__create_listing_dir_containing_non_listing_file()
 
     def __create_listing_dir_containing_non_listing_file(self):
@@ -36,19 +30,16 @@ class TestListingExtractor(unittest.TestCase):
         with open(os.path.join(unclean_listing_directory, "not_a_listing.txt"), "wt") as f:
             f.write("Not a listing")
 
-    def tearDown(self):
-        self.temp_dir.cleanup()
-
     def test_invalid_path(self):
         invalid_paths = [os.path.join(self._BASE_DIRECTORY_PATH, "file1"),
                          os.path.join(self._BASE_DIRECTORY_PATH, "not_a_file_or_dir"),
                          os.path.join(self._BASE_DIRECTORY_PATH, "dir1", "dir2", "file1")]
         for path in invalid_paths:
-            self.assertRaises(NotADirectoryError, ListingExtractor, *(path, self.__context))
+            self.assertRaises(NotADirectoryError, ListingExtractor, *(path, self._context))
     
     def _assert_extracted_listings(self, extract_paths, global_dir=True):
         extractor = ListingExtractor(self._BASE_DIRECTORY_PATH, 
-                                     self.__context,
+                                     self._context,
                                      global_listing_directory=global_dir)
         extractor.extract_all_listings()
         for file_path in extract_paths:
@@ -57,8 +48,8 @@ class TestListingExtractor(unittest.TestCase):
         self.assertFalse(os.path.isdir(listing_directory_in_empty_dir))
 
     def _assert_clear_all_listing_extractions(self, extract_paths):
-        self.__listing_extractor.extract_all_listings()
-        self.__listing_extractor.clear_all_listing_extractions()
+        self._listing_extractor.extract_all_listings()
+        self._listing_extractor.clear_all_listing_extractions()
         for file_path in extract_paths:
             self.assertFalse(os.path.exists(file_path))
         for directory_path in self.__listing_directories_that_should_be_deleted_after_clearing():
